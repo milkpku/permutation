c63f622a657a525fc987491b133bd4959676531f
milkpku <milkpku@gmail.com>
2015-10-23 23:57:48 +0800

    finish class mid_num_in

--- mid_num.cpp
+++ mid_num.cpp
@@ -3,7 +3,65 @@
 
 using namespace std;
 
+long fact(int s){
+    if (s<3){
+        return s;
+    }
+    else{
+        return s*fact(s-1);
+    };
+};
+
 mid_num_in::mid_num_in(int s, int r){
-	this->length = s;
-	MAX_RANK = 
+    this->length = s;
+    this->mid_length = s-1;
+    MAX_RANK = fact(s);
+    this.mid_num = new int[mid_length];
+    
+    if !this->set_rank(r){
+        printf("Failed when instance mid_num_in!\n");
+    }
+};
+
+mid_num_in::set_rank(int r){
+    // judge if r is valid
+    if (!valid_rank(r)){
+        printf('Failed when setting rank!\n');
+        return false;
+    };
+
+    // rank to mid_num procedure
+    this->rank = r;
+    for (int i = 2; i<length+1; i++){
+        mid_num[length - i] = r % i;
+        r /= i;
+    };
+
+    return true;
+}
+
+mid_num_in::valid_rank(long r){
+    if (r>this->MAX_RANK)or(r<0){
+        printf('Invalid input r!\n');
+        return false;
+    };
+    return true;
+};
+
+bool alter(int amount){
+    if (!valid_rank(rank+amount)){
+        printf("Alter too large!\n");
+        return false;
+    };
+
+    this->rank = r + amount;
+    // rank to mid_num procedure
+    int p = mid_length-1;
+    do{
+        mid_num[p] += amount;
+        amount = mid_num[p] / (length - p);
+        mid_num[p] %= (length - p);
+    } while (amount!=0);
+
+    return true;
 }
--- mid_num.h
+++ mid_num.h
@@ -2,47 +2,29 @@
 #define MID_NUM
 
 class mid_num_in{
-private:
-    int length;		       // length of mid_num order
-    long MAX_RANK;	       // max rank of the order can acheave
-    long rank;		       // current rank 
+protected:
+    int mid_length;        // length of mid_num order
+    int length;            // length of whole order, length = mid_length + 1
+    long MAX_RANK;         // max rank of the order can acheave
+    long rank;             // current rank 
     int* mid_num;          // mid num order
 
-    void rank_to_mid();    // convert rank to mid_num order
-    void mid_to_rank();    // convert mid_num order to rank
+    // void rank_to_mid();    // convert rank to mid_num order
+    // void mid_to_rank();    // convert mid_num order to rank
 
-    bool valid_rank();  
-    bool valid_mid_num();
+    // bool valid_rank(long r);     // exam rank number  
+    // bool valid_mid_num();  // exam mid_num number
 
 public:
     mid_num_in(int s, int r = 0);
-    mid_num_in(int s, int* order);
+    // mid_num_in(int s, int* order);
 
     bool set_rank(int r);
-    bool set_mid_num(int* order);+
+    // bool set_mid_num(int* order);
     bool alter(int amount);
 };
 
 class mid_num_de{
-private:
-    int length;
-    long MAX_RANK;
-    long rank;
-    int* mid_num;
-
-    void rank_to_mid();
-    void mid_to_rank();
-
-    bool valid_rank();
-    bool valid_mid_num();
-
-public:
-    mid_num_de(int s, int r = 0);
-    mid_num_de(int s, int* order);
-
-    bool set_rank(int r);
-    bool set_mid_num(int* order);
-    bool alter(int amount);
 };
 
 
