02a73996f134b6a6f1923209a9b8ba42b2deaeb6
milkpku <milkpku@gmail.com>
2015-10-26 11:03:13 +0800

    inherite from rank class

--- mid_num.cpp
+++ mid_num.cpp
@@ -12,18 +12,26 @@ long fact(int s){
     };
 };
 
-mid_num_in::mid_num_in(int s, int r){
+rank::rank(int s, long r){
     this->length = s;
-    this->mid_length = s-1;
     MAX_RANK = fact(s);
-    this.mid_num = new int[mid_length];
-    
-    if !this->set_rank(r){
-        printf("Failed when instance mid_num_in!\n");
-    }
+    set_rank(r);
 };
 
-mid_num_in::set_rank(int r){
+bool rank::valid_rank(long r){
+    if (r >= this->MAX_RANK){
+        printf('Invalid input r! It is too large.\n');
+        return false;
+    };
+    if (r >= this->MAX_RANK)or(r < 0){
+        printf('Invalid input r! It is negetive.\n');
+        return false;
+    };
+
+    return true;
+};
+
+bool rank::set_rank(int r){
     // judge if r is valid
     if (!valid_rank(r)){
         printf('Failed when setting rank!\n');
@@ -32,6 +40,26 @@ mid_num_in::set_rank(int r){
 
     // rank to mid_num procedure
     this->rank = r;
+    return true;
+};
+
+mid_num_in::mid_num_in(int s, long r):rank(s,r)
+{
+    this->mid_length = s-1;
+    this->mid_num = new int[mid_length];
+    
+    if (!this->set_mid(r)){
+        printf("Failed when instance mid_num_in!\n");
+    }
+};
+
+bool mid_num_in::set_mid(int r){
+    // judge if r is valid
+    if (!this->set_rank(r)){
+        return false;
+    };
+
+    // rank to mid_num procedure
     for (int i = 2; i<length+1; i++){
         mid_num[length - i] = r % i;
         r /= i;
@@ -40,21 +68,12 @@ mid_num_in::set_rank(int r){
     return true;
 }
 
-mid_num_in::valid_rank(long r){
-    if (r>this->MAX_RANK)or(r<0){
-        printf('Invalid input r!\n');
-        return false;
-    };
-    return true;
-};
-
-bool alter(int amount){
-    if (!valid_rank(rank+amount)){
-        printf("Alter too large!\n");
+bool mid_num_in::alter(int amount){
+    if (!this->set_rank(rank+amount)){
+        printf("Failed when changing mid_num_in!\n");
         return false;
     };
 
-    this->rank = r + amount;
     // rank to mid_num procedure
     int p = mid_length-1;
     do{
